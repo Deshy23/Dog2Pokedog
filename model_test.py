51131a05dadbefcbdc212b3062fc21a6bec30116
Deshy23 <50605153+Deshy23@users.noreply.github.com>
2022-03-09 09:09:50 -0800

    Add files via upload

---
+++ model_test.py
@@ -0,0 +1,266 @@
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.utils import make_grid
+import torch.utils.data as data
+import matplotlib.pyplot as plt
+from torch.cuda.amp import autocast
+import numpy as np
+import glob
+import random
+import os
+from torch.utils.data import Dataset
+from PIL import Image
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision import transforms
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import torch.nn.functional as F
+torch.manual_seed(0)
+
+#see test function at bottom of file for testing
+
+# Custom Dataloader
+class ImageDataset(data.Dataset):
+    def __init__(self, data_rootA, data_rootB, transform=None):
+        self.transform = transform
+        self.files_A = []
+        self.files_B = []
+        for path, subdirs, files in os.walk(data_rootA):
+            for name in files:
+                if name.endswith(".jpg"):
+                    self.files_A.append(os.path.join(path, name))
+        for path, subdirs, files in os.walk(data_rootB):
+            for name in files:
+                if name.endswith(".jpg"):
+                    self.files_B.append(os.path.join(path, name))
+        if len(self.files_A) > len(self.files_B):
+            self.files_A, self.files_B = self.files_B, self.files_A
+        self.new_perm()
+        assert len(self.files_A) > 0, "Make sure you downloaded the images!"
+
+    def new_perm(self):
+        self.randperm = torch.randperm(len(self.files_B))[:len(self.files_A)]
+
+    def __getitem__(self, index):
+        item_A = self.transform(Image.open(self.files_A[index % len(self.files_A)]))
+        item_B = self.transform(Image.open(self.files_B[self.randperm[index]]))
+        if index == len(self) - 1:
+            self.new_perm()
+        return (item_A - 0.5) * 2, (item_B - 0.5) * 2
+
+    def __len__(self):
+        return min(len(self.files_A), len(self.files_B))
+
+n_epochs = 1
+dim_A = 3
+dim_B = 3
+display_step = 100
+batch_size = 2
+lr = 0.0001
+load_shape = 286
+target_shape = 128
+device = 'cuda'
+
+class ResidualBlock(nn.Module):
+    '''
+    ResidualBlock Class:
+    Performs two convolutions and an instance normalization, the input is added
+    to this output to form the residual block output.
+    Values:
+        input_channels: the number of channels to expect from a given input
+    '''
+    def __init__(self, input_channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, input_channels, kernel_size=3, padding=1, padding_mode='reflect')
+        self.conv2 = nn.Conv2d(input_channels, input_channels, kernel_size=3, padding=1, padding_mode='reflect')
+        self.instancenorm = nn.InstanceNorm2d(input_channels)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        '''
+        Function for completing a forward pass of ResidualBlock: 
+        Given an image tensor, completes a residual block and returns the transformed tensor.
+        Parameters:
+            x: image tensor of shape (batch size, channels, height, width)
+        '''
+        original_x = x.clone()
+        x = self.conv1(x)
+        x = self.instancenorm(x)
+        x = self.activation(x)
+        x = self.conv2(x)
+        x = self.instancenorm(x)
+        return original_x + x
+
+class ContractingBlock(nn.Module):
+    '''
+    ContractingBlock Class
+    Performs a convolution followed by a max pool operation and an optional instance norm.
+    Values:
+        input_channels: the number of channels to expect from a given input
+    '''
+    def __init__(self, input_channels, use_bn=True, kernel_size=3, activation='relu'):
+        super(ContractingBlock, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels, input_channels * 2, kernel_size=kernel_size, padding=1, stride=2, padding_mode='reflect')
+        self.activation = nn.ReLU() if activation == 'relu' else nn.LeakyReLU(0.2)
+        if use_bn:
+            self.instancenorm = nn.InstanceNorm2d(input_channels * 2)
+        self.use_bn = use_bn
+
+    def forward(self, x):
+        '''
+        Function for completing a forward pass of ContractingBlock: 
+        Given an image tensor, completes a contracting block and returns the transformed tensor.
+        Parameters:
+            x: image tensor of shape (batch size, channels, height, width)
+        '''
+        x = self.conv1(x)
+        if self.use_bn:
+            x = self.instancenorm(x)
+        x = self.activation(x)
+        return x
+
+class ExpandingBlock(nn.Module):
+    '''
+    ExpandingBlock Class:
+    Performs a convolutional transpose operation in order to upsample, 
+        with an optional instance norm
+    Values:
+        input_channels: the number of channels to expect from a given input
+    '''
+    def __init__(self, input_channels, use_bn=True):
+        super(ExpandingBlock, self).__init__()
+        self.conv1 = nn.ConvTranspose2d(input_channels, input_channels // 2, kernel_size=3, stride=2, padding=1, output_padding=1)
+        if use_bn:
+            self.instancenorm = nn.InstanceNorm2d(input_channels // 2)
+        self.use_bn = use_bn
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        '''
+        Function for completing a forward pass of ExpandingBlock: 
+        Given an image tensor, completes an expanding block and returns the transformed tensor.
+        Parameters:
+            x: image tensor of shape (batch size, channels, height, width)
+            skip_con_x: the image tensor from the contracting path (from the opposing block of x)
+                    for the skip connection
+        '''
+        x = self.conv1(x)
+        if self.use_bn:
+            x = self.instancenorm(x)
+        x = self.activation(x)
+        return x
+
+class FeatureMapBlock(nn.Module):
+    '''
+    FeatureMapBlock Class
+    The final layer of a Generator - 
+    maps each the output to the desired number of output channels
+    Values:
+        input_channels: the number of channels to expect from a given input
+        output_channels: the number of channels to expect for a given output
+    '''
+    def __init__(self, input_channels, output_channels):
+        super(FeatureMapBlock, self).__init__()
+        self.conv = nn.Conv2d(input_channels, output_channels, kernel_size=7, padding=3, padding_mode='reflect')
+
+    def forward(self, x):
+        '''
+        Function for completing a forward pass of FeatureMapBlock: 
+        Given an image tensor, returns it mapped to the desired number of channels.
+        Parameters:
+            x: image tensor of shape (batch size, channels, height, width)
+        '''
+        x = self.conv(x)
+        return x
+
+class Generator(nn.Module):
+    '''
+    Generator Class
+    A series of 2 contracting blocks, 9 residual blocks, and 2 expanding blocks to 
+    transform an input image into an image from the other class, with an upfeature
+    layer at the start and a downfeature layer at the end.
+    Values:
+        input_channels: the number of channels to expect from a given input
+        output_channels: the number of channels to expect for a given output
+    '''
+    def __init__(self, input_channels, output_channels, hidden_channels=64):
+        super(Generator, self).__init__()
+        self.upfeature = FeatureMapBlock(input_channels, hidden_channels)
+        self.contract1 = ContractingBlock(hidden_channels)
+        self.contract2 = ContractingBlock(hidden_channels * 2)
+        res_mult = 4
+        self.res0 = ResidualBlock(hidden_channels * res_mult)
+        self.res1 = ResidualBlock(hidden_channels * res_mult)
+        self.res2 = ResidualBlock(hidden_channels * res_mult)
+        self.res3 = ResidualBlock(hidden_channels * res_mult)
+        self.res4 = ResidualBlock(hidden_channels * res_mult)
+        self.res5 = ResidualBlock(hidden_channels * res_mult)
+        self.res6 = ResidualBlock(hidden_channels * res_mult)
+        self.res7 = ResidualBlock(hidden_channels * res_mult)
+        self.res8 = ResidualBlock(hidden_channels * res_mult)
+        self.expand2 = ExpandingBlock(hidden_channels * 4)
+        self.expand3 = ExpandingBlock(hidden_channels * 2)
+        self.downfeature = FeatureMapBlock(hidden_channels, output_channels)
+        self.tanh = torch.nn.Tanh()
+
+    def forward(self, x):
+        '''
+        Function for completing a forward pass of Generator: 
+        Given an image tensor, passes it through the U-Net with residual blocks
+        and returns the output.
+        Parameters:
+            x: image tensor of shape (batch size, channels, height, width)
+        '''
+        x0 = self.upfeature(x)
+        x1 = self.contract1(x0)
+        x2 = self.contract2(x1)
+        x3 = self.res0(x2)
+        x4 = self.res1(x3)
+        x5 = self.res2(x4)
+        x6 = self.res3(x5)
+        x7 = self.res4(x6)
+        x8 = self.res5(x7)
+        x9 = self.res6(x8)
+        x10 = self.res7(x9)
+        x11 = self.res8(x10)
+        x12 = self.expand2(x11)
+        x13 = self.expand3(x12)
+        xn = self.downfeature(x13)
+        return self.tanh(xn)
+
+gen_AB = Generator(dim_A, dim_B).to(device)
+gen_BA = Generator(dim_B, dim_A).to(device)
+gen_opt = torch.optim.Adam(list(gen_AB.parameters()) + list(gen_BA.parameters()), lr=lr, betas=(0.5, 0.999))
+
+# Feel free to change pretrained to False if you're training the model from scratch
+pretrained = True
+if pretrained:
+    pre_dict = torch.load('name.pth') #<-select model to test
+    gen_AB.load_state_dict(pre_dict['gen_AB'])
+    gen_BA.load_state_dict(pre_dict['gen_BA'])
+    gen_opt.load_state_dict(pre_dict['gen_opt'])
+
+from skimage import color
+import numpy as np
+plt.rcParams["figure.figsize"] = (10, 10)
+
+def test():
+    transform_test = transforms.Compose([
+    transforms.Lambda(lambda img: transforms.ToTensor()(img)[:3,:, :]),
+    transforms.Resize(size = (512,512)),
+    transforms.Normalize(mean=(0.5,),std=(0.5,)),
+    ])
+    image_name = 'goldenwhiteground' # replace with image name(no extension) including path if necessary
+    image_extension = 'jpg' #replace with image extension
+    im = Image.open(image_name + '.' + image_extension)
+    r = transforms.Normalize(mean=(0.5,),std=(0.5,))(gen_BA(transform_test(im).unsqueeze(0).to(device))).to('cpu') 
+    plt.figure(figsize = (10,10))
+    im = transforms.ToPILImage()(r.squeeze(0))
+    plt.imshow(im)
+    plt.show()
+test()
